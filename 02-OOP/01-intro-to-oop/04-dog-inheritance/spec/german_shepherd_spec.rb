0078f4a33d5e2ba559334b1aa09f3aebe824a5fb
Geoff Hubbard <geoff.hubbard@karnovgroup.com>
2022-08-18 09:03:04 +0200

    Change the german_shepherd spec
    Implement the GermanShepherd spec so that it tests for the things that
    GermanShepherd can do before worrying about how it does them and leave
    the inheritance test for last.
    
    This allows the student to implement a basic working solution and then
    refactor it to use inheritance.

--- 02-OOP/01-intro-to-oop/04-dog-inheritance/spec/german_shepherd_spec.rb
+++ 02-OOP/01-intro-to-oop/04-dog-inheritance/spec/german_shepherd_spec.rb
@@ -1,14 +1,23 @@
-require 'german_shepherd'
+require "german_shepherd"
 
 describe GermanShepherd do
+  describe "bark" do
+    it "can bark" do
+      german_shepherd = GermanShepherd.new
+      expect(german_shepherd.bark).to eq("woof woof")
+    end
+  end
 
-  describe 'inheritance' do
-    it 'should add Dog as a superclass of GermanShepherd' do
-      expect(GermanShepherd.superclass).to eq(Dog)
+  describe "inheritance" do
+    it "should bark like a dog" do
+      german_shepherd = GermanShepherd.new
+      dog = Dog.new
+
+      expect(german_shepherd.bark).to eq(dog.bark)
     end
 
-    it 'should extend GermanShepherd with the instance methods of Dog' do
-      expect(GermanShepherd.instance_methods).to include(:bark)
+    it "should add Dog as a superclass of GermanShepherd" do
+      expect(GermanShepherd.superclass).to eq(Dog)
     end
   end
 end
