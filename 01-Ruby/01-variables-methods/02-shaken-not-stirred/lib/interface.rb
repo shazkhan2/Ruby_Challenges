46bef9fe7bcbf7fe220833b5a7014e80c6789c12
shazkhan2 <seewardak@hotmail.com>
2024-09-17 17:59:31 +0200

    deleted spy from the parameter

--- 01-Ruby/01-variables-methods/02-shaken-not-stirred/lib/interface.rb
+++ 01-Ruby/01-variables-methods/02-shaken-not-stirred/lib/interface.rb
@@ -4,11 +4,11 @@ require_relative 'shaken_not_stirred'
 puts "Welcome to the MI6 spy introduction generator"
 
 puts "What's your first name? "
-spy_first_name = gets.chomp
+first_name = gets.chomp
 puts "What's your last name?"
-spy_last_name = gets.chomp
+last_name = gets.chomp
 
 puts "Thanks for the information."
 
 puts "Henceforth, all emails you send will begin with: "
-puts introduce_spy(spy_first_name, spy_last_name)
+puts introduce_spy(first_name, last_name)
