852a0182d7a1172dc4282c63cfa7f3d8145285b3
shazkhan2 <seewardak@hotmail.com>
2024-09-17 17:54:48 +0200

    Update to fit the rake requirement

--- 01-Ruby/01-variables-methods/02-shaken-not-stirred/lib/interface.rb
+++ 01-Ruby/01-variables-methods/02-shaken-not-stirred/lib/interface.rb
@@ -1,5 +1,6 @@
 require_relative 'shaken_not_stirred'
 
+
 puts "Welcome to the MI6 spy introduction generator"
 
 puts "What's your first name? "
@@ -9,4 +10,5 @@ spy_last_name = gets.chomp
 
 puts "Thanks for the information."
 
-puts "Henceforth, all emails you send will begin with: #{introduce_spy(spy_first_name, spy_last_name)}"
+puts "Henceforth, all emails you send will begin with: "
+puts introduce_spy(spy_first_name, spy_last_name)
