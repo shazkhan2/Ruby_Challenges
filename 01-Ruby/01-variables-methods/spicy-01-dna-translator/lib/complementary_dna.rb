6dffe39a318d1df1f2a572ed36dc0e0b8e0116aa
shazkhan2 <seewardak@hotmail.com>
2024-09-21 13:31:07 +0200

    spicy-dna translator done

--- 01-Ruby/01-variables-methods/spicy-01-dna-translator/lib/complementary_dna.rb
+++ 01-Ruby/01-variables-methods/spicy-01-dna-translator/lib/complementary_dna.rb
@@ -1,3 +1,25 @@
 def complementary_dna(dna_strand)
-  # TODO: your code here
+
+  if dna_strand.match?(/[^ATCGatcg]/)
+    return "invalid dna strand detected"
+  end
+
+  dna_strand = dna_strand.upcase
+
+  pairs = {
+    'A' => 'T',
+    'T' => 'A',
+    'C' => 'G',
+    'G' => 'C'
+  }
+
+  complementary_dna = dna_strand.chars.map { |char| pairs[char] }.join
+
+  return complementary_dna
 end
+
+# irb test 
+puts complementary_dna("ACGT")    
+puts complementary_dna("acgt")    
+puts complementary_dna("aCgT")    
+puts complementary_dna("aCgT123") 
