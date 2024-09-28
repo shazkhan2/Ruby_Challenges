9161b65f3dd8e18d653c92c8b09b59857cbf4748
shazkhan2 <seewardak@hotmail.com>
2024-09-28 16:15:04 +0200

    Morse code done

--- 01-Ruby/01-variables-methods/spicy-03-morse-code/lib/encoder.rb
+++ 01-Ruby/01-variables-methods/spicy-03-morse-code/lib/encoder.rb
@@ -1,4 +1,24 @@
 def encode(text)
   # TODO: write a method which returns the morse sentence for the given text
   # NOTE: you may want to create a `encode_word` method
+
+  morse_code = {
+    'A' => '.-',    'B' => '-...',  'C' => '-.-.',  'D' => '-..',   'E' => '.',
+    'F' => '..-.',  'G' => '--.',   'H' => '....',  'I' => '..',    'J' => '.---',
+    'K' => '-.-',   'L' => '.-..',  'M' => '--',    'N' => '-.',    'O' => '---',
+    'P' => '.--.',  'Q' => '--.-',  'R' => '.-.',   'S' => '...',   'T' => '-',
+    'U' => '..-',   'V' => '...-',  'W' => '.--',   'X' => '-..-',  'Y' => '-.--',
+    'Z' => '--..'
+  }
+
+  text_processed = text.upcase.gsub(/[^A-Z\s]/, '')
+    morse_words = text_processed.split(" ").map do |word|
+    word.chars.map { |char| morse_code[char] }.join(" ")
 end
+
+encode_word = morse_words.join('|')
+return encode_word
+
+end
+
+puts encode("Hey Jude, don't make it bad")
