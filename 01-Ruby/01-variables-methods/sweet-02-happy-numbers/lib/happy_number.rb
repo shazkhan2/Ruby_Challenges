d776f2958e5d54d7af51949e5426a6d75971148d
shazkhan2 <seewardak@hotmail.com>
2024-09-29 14:46:06 +0200

    Happy number done

--- 01-Ruby/01-variables-methods/sweet-02-happy-numbers/lib/happy_number.rb
+++ 01-Ruby/01-variables-methods/sweet-02-happy-numbers/lib/happy_number.rb
@@ -1,3 +1,13 @@
+# - First, you take the sum of all its digits.
+# - Second, you reverse that sum's digits
+# - Third, you multiply the sum with its reverse
+# - If the the result of that multiplication is equal to the number, the number is a happy number.
+
 def happy_number?(number)
   # TODO: your code here
-end
+    sum = number.to_s.chars.map(&:to_i).sum
+    reversed = sum.to_s.reverse.to_i
+    multiply = sum * reversed
+    multiply == number
+  end
+  
