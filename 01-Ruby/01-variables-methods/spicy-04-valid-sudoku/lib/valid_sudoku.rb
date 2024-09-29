f5384ece6edd06df4b138afb7f3f54c112de4756
shazkhan2 <seewardak@hotmail.com>
2024-09-29 14:14:42 +0200

    Sudoku completed

--- 01-Ruby/01-variables-methods/spicy-04-valid-sudoku/lib/valid_sudoku.rb
+++ 01-Ruby/01-variables-methods/spicy-04-valid-sudoku/lib/valid_sudoku.rb
@@ -1,7 +1,7 @@
 def valid_section?(section)
-  # TODO: your code here. There should be an array from 1-9 with no repetition. integers
-section.uniq.length ==9 && section.sort == (1..9).to_a
-
+  #   # TODO: your code here. There should be an array from 1-9 with no repetition. integers
+  valid_section = ->(section) { section.uniq.size == 9 }
+  valid_section.call(section)
 end
 
 def vertical_lines(board)
@@ -17,47 +17,67 @@ end
 # end
 
 def regions(board)
-result = []
-
-(0..8).each do |matrix|
-row_index = (matrix / 3) * 3
-col_index = (matrix % 3 ) *3
+  (0..8).map do |matrix|  
+    row_index = (matrix / 3) * 3
+    col_index = (matrix % 3) * 3
 
     region = []
     (0..2).each do |i|
       (0..2).each do |j|
-        region.push(board[row_index +i][col_index + j])
+        region.push(board[row_index + i][col_index + j])
       end
     end
-    result.push(region)
+    region  
+  end
 end
 
-result
-end
 
-
-def valid_sudoku?(board)
+# def valid_sudoku?(board)
   # TODO: your code here. Each 3x3 grid has unique numbers. Integers.
-end
+  def regions(board)
+    (0..8).map do |matrix|  
+      row_index = (matrix / 3) * 3
+      col_index = (matrix % 3) * 3
+  
+      region = []
+      (0..2).each do |i|
+        (0..2).each do |j|
+          region.push(board[row_index + i][col_index + j])
+        end
+      end
+      region  
+    end
+  end
+  
+  def valid_sudoku?(board)
+    valid_section = ->(section) { section.uniq.size == section.size }
+  
+    board.all?(&valid_section) &&                  
+      board.transpose.all?(&valid_section) &&     
+      regions(board).all?(&valid_section)          
+  end
+  
+  
 
-puts valid_section?([1,2,3,4,5,6,7,8,9])
-puts valid_section?([1,2,3,4,5,6,7,8,9,1])
-puts vertical_lines ([ [5, 3, 4, 6, 7, 8, 9, 1, 2],
-[6, 7, 2, 1, 9, 5, 3, 4, 8],
-[1, 9, 8, 3, 4, 2, 5, 6, 7],
-[8, 5, 9, 7, 6, 1, 4, 2, 3],
-[4, 2, 6, 8, 5, 3, 7, 9, 1],
-[7, 1, 3, 9, 2, 4, 8, 5, 6],
-[9, 6, 1, 5, 3, 7, 2, 8, 4],
-[2, 8, 7, 4, 1, 9, 6, 3, 5],
-[3, 4, 5, 2, 8, 6, 1, 7, 9]])
 
-puts regions ([["a", "a", "a", "b", "b", "b", "c", "c", "c"],
-["a", "a", "a", "b", "b", "b", "c", "c", "c"],
-["a", "a", "a", "b", "b", "b", "c", "c", "c"],
-["d", "d", "d", "e", "e", "e", "f", "f", "f"],
-["d", "d", "d", "e", "e", "e", "f", "f", "f"],
-["d", "d", "d", "e", "e", "e", "f", "f", "f"],
-["g", "g", "g", "h", "h", "h", "i", "i", "i"],
-["g", "g", "g", "h", "h", "h", "i", "i", "i"],
-["g", "g", "g", "h", "h", "h", "i", "i", "i"]])
+# puts valid_section?([1,2,3,4,5,6,7,8,9])
+# puts valid_section?([1,2,3,4,5,6,7,8,9,1])
+# puts vertical_lines ([ [5, 3, 4, 6, 7, 8, 9, 1, 2],
+# [6, 7, 2, 1, 9, 5, 3, 4, 8],
+# [1, 9, 8, 3, 4, 2, 5, 6, 7],
+# [8, 5, 9, 7, 6, 1, 4, 2, 3],
+# [4, 2, 6, 8, 5, 3, 7, 9, 1],
+# [7, 1, 3, 9, 2, 4, 8, 5, 6],
+# [9, 6, 1, 5, 3, 7, 2, 8, 4],
+# [2, 8, 7, 4, 1, 9, 6, 3, 5],
+# [3, 4, 5, 2, 8, 6, 1, 7, 9]])
+
+# puts regions ([["a", "a", "a", "b", "b", "b", "c", "c", "c"],
+# ["a", "a", "a", "b", "b", "b", "c", "c", "c"],
+# ["a", "a", "a", "b", "b", "b", "c", "c", "c"],
+# ["d", "d", "d", "e", "e", "e", "f", "f", "f"],
+# ["d", "d", "d", "e", "e", "e", "f", "f", "f"],
+# ["d", "d", "d", "e", "e", "e", "f", "f", "f"],
+# ["g", "g", "g", "h", "h", "h", "i", "i", "i"],
+# ["g", "g", "g", "h", "h", "h", "i", "i", "i"],
+# ["g", "g", "g", "h", "h", "h", "i", "i", "i"]])
